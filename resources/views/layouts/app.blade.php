1c1ca616be4e40ffba1b2bb8d0307f9596e43ead
pestovmv <pestovmv@inbox.ru>
2020-09-15 11:07:38 +0300

    init

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="page">
+    <div class="page__vue-application js-page__vue-application">
+        <header class="page__header">
+            <h1>
+
+            </h1>
+        </header>
+        <main class="page__content">
+            @yield('content')
+        </main>
+        <footer class="page__footer">
+            footer
+        </footer>
+    </div>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</body>
+</html>
